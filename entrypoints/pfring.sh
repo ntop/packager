c5e3a341e174c1daf931df69b92b7ef71e5335a8
Simone Mainardi <mainardi@ntop.org>
2016-09-08 17:46:04 +0200

    Resets pfring tests

--- entrypoints/pfring.sh
+++ entrypoints/pfring.sh
@@ -7,8 +7,7 @@ set -e
 
 if [ "$1" = 'test' ]; then
     # do interesting test stuff here
-    modprobe pf_ring
-    rmmod pf_ring
+    echo "<test to be decided>"
 else
     # can use this to run ntopng in the background for example
     exec "$@"
